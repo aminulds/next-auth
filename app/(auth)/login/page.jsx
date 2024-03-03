53a32eb5da9bd282fe7c3d63f8de7ef3b9a86141
aminulds <aminulds61@gmail.com>
2024-03-03 23:59:05 +0600

    prisma db setup

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {LoginForm} from "../_components/login-form";
-
-const LoginPage = () => {
+const LoginPage = async () => {
     return (
         <div>
             <LoginForm />
