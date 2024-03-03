de4ec525e9a56aff936a137a13b5fbece1bd673b
aminulds <aminulds61@gmail.com>
2024-03-03 22:18:08 +0600

    register form created

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const LoginSchema = z.object({
     email: z.string().email().min(1, "Email is required."),
     password: z.string().min(1, "Password is required.")
 });
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, "Name is required."),
+    email: z.string().email().min(1, "Email is required."),
+    password: z.string().min(6, "Password must 6 character long.")
+});
